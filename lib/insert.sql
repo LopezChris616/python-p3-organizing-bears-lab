7b97f9dfbb636d41bb314f7eef5f0259945f1e96
Christian Lopez <lopezchristian1610@gmail.com>
2023-08-09 02:47:04 -0400

    All sql tests passed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "brown", "wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "F", "brown", "wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "brown", "wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 2, "F", "brown", "wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "brown", "wild", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "M", "brown", "wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "F", "brown", "wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, "M", "brown", "wild", 0);
